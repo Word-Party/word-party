841849caf99534a9b04a5024e63969e1494a467b
Midian Judah <midianjudah@gmail.com>
2022-07-12 19:18:28 -0400

    added team to about section

--- src/About.js
+++ src/About.js
@@ -19,32 +19,7 @@ function About() {
             </div>
           </li>
 
-          <li>
-            <img src={require("./assets/midian-judah.jpeg")} alt="Team member Midian Judah"/>
-            <p className="dev-name">Midian Judah</p>
-            <div className="icon-container">
-            <a href="https://github.com/midianjudah"><i class="fa-brands fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/midian-judah-25aa36163/"><i class="fa-brands fa-linkedin"></i></a>
-            </div>
-          </li>
-
-          <li>
-            <img src="http://placekitten.com/200/300" alt="Team member Robert Mason"/>
-            <p className="dev-name">Robert Mason</p>
-            <div className="icon-container">
-            <a href="https://github.com/rslmason"><i class="fa-brands fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/rslmason/"><i class="fa-brands fa-linkedin"></i></a>
-            </div>
-          </li>
-
-          <li>
-          <img src={require("./assets/simon-oconnor.jpg")} alt="Team member Simon O'Connor"/>
-            <p className="dev-name">Simon O'Connor</p>
-            <div className="icon-container">
-            <a href="https://github.com/SimonOConnor28"><i class="fa-brands fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/simontoconnor/"><i class="fa-brands fa-linkedin"></i></a>
-            </div>
-          </li>
+          
           
         </ul>
       </div>
